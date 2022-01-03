0fe5c0c8a086518a88d1b566bad166c05e0df5de
ritartha017 <ritartha017@bk.ru>
2022-01-03 20:30:14 +0200

    added unpushed labs

---
+++ 7_a_templates.cpp
@@ -0,0 +1,2 @@
+// Creaţi o funcţie şablon, de sortare în creştere după metoda bulelor.
+// Funcţia trebuie să lucreze cu masive de lungimi diferite.
---
+++ 7_b_templates.cpp
@@ -0,0 +1,3 @@
+// Creaţi clasa parametrizată Map – listă asociativă, care conţine cheia câmpului şi valoarea.
+// Unei chei îi corespunde o valoarea.  Clasa trebuie să conţină constructorii, destructorii şi  funcţiile add,
+// removeByKey, getLength, getByKey, getByValue, operatorii [] şi operatorii de intrare/ieşire.
